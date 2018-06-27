c96fd79ba7abcc0b4a0066edf6aedb4cf60105d1
Sophie Haight <shaight737@gmail.com>
2018-06-27 11:48:41 -0500

    add boring commentary

--- temp.py
+++ temp.py
@@ -1,3 +1,5 @@
 import numpy as np 
 print("Did you know 2+2=4")
-print("Why does python 3 work like this I dont like parenthese")i
+
+print("Why does python 3 work like this I dont like parenthese")
+print("I've made a change")
